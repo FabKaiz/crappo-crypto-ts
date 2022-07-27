748c2b68bd3e0e57b6605c41da894299d7579528
Fabien Chareun <cacrfabfr@gmail.com>
2022-07-27 11:34:58 +0200

    tsx done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,29 @@
 import Image from 'next/image'
 import logo from '../public/assets/Logo.png'
+import styles from '../styles/Navbar.module.scss'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-      <Image src={logo} alt="logo" />
-    </div>
+    <nav className={styles.navContainer}>
+      <div>
+        <Image src={logo} alt="logo" />
+      </div>
+
+      <div className={styles.navLinksContainer}>
+        <div className={styles.navLinks}>
+          <ul>
+            <li>Products</li>
+            <li>Features</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className={styles.navBtns}>
+          <a className={styles.loginBtn}>Login</a> |{' '}
+          <button className={styles.registerBtn}>Register</button>
+        </div>
+      </div>
+    </nav>
   )
 }
 
