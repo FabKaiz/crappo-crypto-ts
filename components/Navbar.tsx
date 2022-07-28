694b0e95b78fff66a313358b4268e7dc5b47dd58
Fabien Chareun <cacrfabfr@gmail.com>
2022-07-28 12:05:57 +0200

    added burger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import logo from '../public/assets/Logo.png'
 import styles from '../styles/Navbar.module.scss'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+
   return (
     <nav className={styles.navContainer}>
-      <div>
+      <div className={styles.imgContainer}>
         <Image src={logo} alt="logo" />
       </div>
 
@@ -22,6 +26,27 @@ const Navbar = () => {
           <a className={styles.loginBtn}>Login</a> |{' '}
           <button className={styles.registerBtn}>Register</button>
         </div>
+        <GiHamburgerMenu
+          onClick={() => setToggleMenu(!toggleMenu)}
+          className={styles.burgerMenuBtn}
+        />
+
+        {toggleMenu && (
+          <div className={styles.navMenu}>
+            <ul>
+              <li>
+                <a href="#" onClick={() => setToggleMenu(!toggleMenu)}>
+                  Login
+                </a>
+              </li>
+              <li>
+                <a href="#" onClick={() => setToggleMenu(!toggleMenu)}>
+                  Register
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   )
