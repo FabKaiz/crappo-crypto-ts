27b37e3029669414adfbd8c8f61ecae49bc59a2f
Fabien Chareun <cacrfabfr@gmail.com>
2022-07-29 11:07:54 +0200

    Navbar format

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,6 +21,7 @@ const Navbar = () => {
             <li>About</li>
             <li>Contact</li>
           </ul>
+          
         </div>
         <div className={styles.navBtns}>
           <a className={styles.loginBtn}>Login</a> |{' '}
