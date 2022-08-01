ed86cf746a13ea8916181e271dc33a87eb4bc535
Fabien Chareun <cacrfabfr@gmail.com>
2022-08-01 11:16:01 +0200

    tradecard tsx

---
+++ components/TradeCard.tsx
@@ -0,0 +1,38 @@
+import Image, { StaticImageData } from 'next/image'
+import styles from '../styles/TradeCard.module.scss'
+import { AiOutlineRight } from 'react-icons/ai'
+
+type TradeProps = {
+  logo: string | StaticImageData
+  title: string
+  abbreviations: string
+  description: string
+  active: boolean
+}
+
+const TradeCard: React.FC<TradeProps> = ({
+  logo,
+  title,
+  abbreviations,
+  description,
+  active,
+}) => {
+  return (
+    <div className={`${styles.tradeCardContainer} ${active ? styles.darkBg : styles.lightBg}`}>
+      <Image src={logo} alt={`${title} logo`} />
+      <h2>{title} <span>{abbreviations}</span></h2>
+      <p>{description}</p>
+      {active ? (
+        <button className="mainButton">
+          Start mining <AiOutlineRight />
+        </button>
+      ) : (
+        <button className={styles.buttonRight}>
+          <AiOutlineRight />
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default TradeCard
