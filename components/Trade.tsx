d63a38931d4622f539a4dd1a46a476903e5b9fbe
Fabien Chareun <cacrfabfr@gmail.com>
2022-08-01 12:45:15 +0200

    added section container and select options

--- components/Trade.tsx
+++ components/Trade.tsx
@@ -6,7 +6,7 @@ import ltcLogo from '../public/assets/ltcLogo.png'
 
 const Trade = () => {
   return (
-    <section className={styles.tradeContainer}>
+    <section className={`${styles.tradeContainer} sectionContainer`}>
       <div className={styles.tradeTop}>
         <div className={styles.tradeTopText}>
           <h2>Check how much you can earn</h2>
@@ -18,7 +18,10 @@ const Trade = () => {
         <div className={styles.tradeTopCalculate}>
           <form className={styles.calculateForm}>
             <input type="text" placeholder="Enter your hash rate" />
-            <select name="TH/s" id=""></select>
+            <select name="select">
+              <option value="TH/s">TH/s</option>
+              <option value="TH/h">GH/s</option>
+            </select>
 
             <button>Calculate</button>
           </form>
@@ -33,38 +36,38 @@ const Trade = () => {
           </div>
         </div>
       </div>
-      <div className={styles.tradeBottom}>
-        <h2>Trade securely and market the high growth cryptocurrencies.</h2>
-        <div className={styles.tradeCardsContainer}>
-          <TradeCard
-            logo={btcLogo}
-            title={'Bitcoin'}
-            abbreviations={'BTC'}
-            description={
-              'Digital currency in which a record of transactions is maintained.'
-            }
-            active={true}
-          />
-          <TradeCard
-            logo={ethLogo}
-            title={'Ethereum'}
-            abbreviations={'ETH'}
-            description={
-              'Blockchain technology to create and run decentralized digital applications.'
-            }
-            active={false}
-          />
-          <TradeCard
-            logo={ltcLogo}
-            title={'Litecoin'}
-            abbreviations={'LTC'}
-            description={
-              'Cryptocurrency that enables instant payments to anyone in the world.'
-            }
-            active={false}
-          />
+        <div className={styles.tradeBottom}>
+          <h2>Trade securely and market the high growth cryptocurrencies.</h2>
+          <div className={styles.tradeCardsContainer}>
+            <TradeCard
+              logo={btcLogo}
+              title={'Bitcoin'}
+              abbreviations={'BTC'}
+              description={
+                'Digital currency in which a record of transactions is maintained.'
+              }
+              active={true}
+            />
+            <TradeCard
+              logo={ethLogo}
+              title={'Ethereum'}
+              abbreviations={'ETH'}
+              description={
+                'Blockchain technology to create and run decentralized digital applications.'
+              }
+              active={false}
+            />
+            <TradeCard
+              logo={ltcLogo}
+              title={'Litecoin'}
+              abbreviations={'LTC'}
+              description={
+                'Cryptocurrency that enables instant payments to anyone in the world.'
+              }
+              active={false}
+            />
+          </div>
         </div>
-      </div>
     </section>
   )
 }
